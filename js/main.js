494137ab8ae37dca0ea839d3be0af040009bf56e
Alexandr <stalien@yandex.ru>
2020-10-27 22:17:39 +0300

    homeWork

--- js/main.js
+++ js/main.js
@@ -8,3 +8,87 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+// day 1
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const loginForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('gloDelivery');
+
+//console.log(modalAuth);
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+}
+
+
+
+function authorized() {
+
+  function logOut() {
+    login = null;    
+    localStorage.removeItem('gloDelivery');
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+
+  console.log('Авторизован');
+
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+
+  buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+  console.log('Не авторизован');
+
+  function logIn(event) {
+    event.preventDefault();
+    //console.log('Логин');
+    login = loginInput.value;
+    localStorage.setItem('gloDelivery', login);
+    //console.log(login);
+    toggleModalAuth();
+    buttonAuth.removeEventListener('click', toggleModalAuth);
+    closeAuth.removeEventListener('click', toggleModalAuth);
+    logInForm.removeEventListener('submit', logIn);
+    loginForm.reset();
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInForm.addEventListener('submit', logIn);
+  /*logInForm.addEventListener('submit', function(event) {
+    if (loginInput.value) {
+      logIn;       
+     } else {
+       event.preventDefault();
+       alert('Введите логин');
+     }
+     
+  });*/
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+   notAuthorized();
+  }
+}
+
+checkAuth();
